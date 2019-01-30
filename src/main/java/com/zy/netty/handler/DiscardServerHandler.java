84d086e469383812804aee625bdcf8846d80c637
bigbang555 <zhaoyan448>
2019-01-30 16:23:42 +0800

    让接受的信息打印在控制台，然后再释放

--- src/main/java/com/zy/netty/handler/DiscardServerHandler.java
+++ src/main/java/com/zy/netty/handler/DiscardServerHandler.java
@@ -3,14 +3,24 @@ package com.zy.netty.handler;
 import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.util.ReferenceCountUtil;
 
 public class DiscardServerHandler extends ChannelInboundHandlerAdapter {
 
 
     @Override
     public void channelRead(final ChannelHandlerContext ctx, final Object msg) {
-        //discard the dara received
-        ((ByteBuf) msg).release();
+        ByteBuf in = (ByteBuf) msg;
+
+        try {
+            while (in.isReadable()) {
+                System.out.print((char) in.readByte());
+                System.out.flush();
+            }
+        } finally {
+            //丢弃接受到的数据
+            ReferenceCountUtil.release(msg);
+        }
     }
 
 
@@ -18,7 +28,7 @@ public class DiscardServerHandler extends ChannelInboundHandlerAdapter {
     public void exceptionCaught(final ChannelHandlerContext ctx, final Throwable cause)
             throws Exception {
         cause.printStackTrace();
-        //close the connection when errors occur
+        //当错误发生，中断连接
         ctx.close();
     }
 }
